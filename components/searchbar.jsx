b56f3028cc2c17a9c90c426e5cf7eac09329c4fc
pengkiang <pengkiangtan@gmail.com>
2024-04-09 01:31:53 +0800

    fix search

--- components/searchbar.jsx
+++ components/searchbar.jsx
@@ -1,16 +1,11 @@
-'use client'
-import React, { useEffect, useState } from 'react'
-import { useSearchParams, usePathname, useRouter } from 'next/navigation'
+import React, { useEffect, useState, Suspense } from 'react'
 import { useDebouncedCallback } from "use-debounce"
 import { universalSearch } from '@/utils/supabase/actions'
 
 export default function SearchComponent() {
-  const router = useRouter()
-  const { replace } = useRouter()
   const [query, setQuery] = useState("")
   const [queryResults, setQueryResults] = useState([])
-  const searchParams = useSearchParams()
-  const pathname = usePathname();
+  const [isSearching, setIsSearching] = useState(false)
 
   const handleRedirect = (redirecturl, redirectsection, event) => {
     const params = new URLSearchParams(searchParams)
@@ -41,20 +36,23 @@ export default function SearchComponent() {
 
   useEffect(() => {
     const getResults = async (queryTerm) => {
+      setIsSearching(true)
       try {
         const results = await universalSearch(queryTerm)
         setQueryResults(results)
-      }
-      catch (error) {
+      } catch (error) {
         console.log("error fetching search results", error)
       }
+      setIsSearching(false)
     }
-    getResults(query)
-
+    if (query) {
+      getResults(query)
+    } else {
+      setQueryResults([])
+    }
   }, [query])
 
   return (
-    // TODO fix the results,search bar ui and clicking them to go to the location/page 
     <div className='relative flex flex-col'>
       <input
         id="search"
@@ -65,17 +63,19 @@ export default function SearchComponent() {
         type="search"
         autoComplete='off'
       />
-      {/* display search */}
-      <ul className={`absolute left-0 top-8 w-full mt-2 border-gray-300 rounded shadow-md z-10 ${queryResults?.length && 'bg-white border'}`}>
-        {queryResults?.length > 0 && queryResults.map((result, i) => (
-          <li onClick={() => handleRedirect(result.tag, result.html_id, event)} className='py-2 px-4 hover:bg-gray-100 border-y-[1px] border-grey-100' key={i}>
-            <div className='max-w-full'>
-              <h4 className='font-bold'>{result.title}</h4>
-              <span className='flex flex-wrap max-w-full whitespace-normal text-wrap break-all'>{result.description.length > 100 ? result.description.slice(0, 100) + '...' : result.description}</span>
-            </div>
-          </li>
-        ))}
-      </ul>
+      <Suspense fallback={<div>Loading...</div>}>
+        <ul className={`absolute left-0 top-8 w-full mt-2 border-gray-300 rounded shadow-md z-10 ${queryResults?.length && 'bg-white border'}`}>
+          {isSearching && <div>Loading...</div>}
+          {!isSearching && queryResults?.length > 0 && queryResults.map((result, i) => (
+            <li onClick={() => handleRedirect(result.tag, result.html_id, event)} className='py-2 px-4 hover:bg-gray-100 border-y-[1px] border-grey-100' key={i}>
+              <div className='max-w-full'>
+                <h4 className='font-bold'>{result.title}</h4>
+                <span className='flex flex-wrap max-w-full whitespace-normal text-wrap break-all'>{result.description.length > 100 ? result.description.slice(0, 100) + '...' : result.description}</span>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </Suspense>
     </div>
   )
 }
