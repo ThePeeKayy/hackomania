59867025ae715f5e640f599b7efffc914b05d9ef
pengkiang <pengkiangtan@gmail.com>
2024-04-09 23:13:04 +0800

    no consolelog

--- components/CreatePostModal.jsx
+++ components/CreatePostModal.jsx
@@ -96,7 +96,6 @@ const CreatePostModal = ({ setLoading, title, setTitle, desc, setDesc, imgUrls, 
                     <input id='uploadFile' type='file' className='hidden' onChange={handleFileUpload} />
 
                     <IoMdSend size={32} className='flex right-0' onClick={(title && desc) ? () => {
-                        console.log(title, desc, imgUrls, user.session.user.id, selectedTags, user.session.user.user_metadata.picture, user.session.user.user_metadata.name)
                         createNetworkPost(title, desc, imgUrls, user.session.user.id, selectedTags, user.session.user.user_metadata.picture, user.session.user.user_metadata.name)
                         setCreateModal(false)
                         setLoading((prev) => !prev)
